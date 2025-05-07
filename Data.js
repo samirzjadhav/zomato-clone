103c56e920265f9e425564879b234638b9130d69
samir jadhav <samirzjadhav@gmail.com>
2025-05-07 14:00:00 +0530

    feat: added more data for food remdering

---
+++ Data.js
@@ -0,0 +1,305 @@
+Restaurants = [
+  {
+    id: "R1",
+    imageSrc: "Image/Restaurants_Products1.avif",
+    offerText: "₹200 OFF",
+    RestaurantsName: "La Pino's Pizza",
+    rating: "4.1",
+    TotalRating: "1156",
+    Diningrating: "4.3",
+    Deliveryrating: "4.0",
+    DeliveryOut: "true",
+    TotalDinigrating: "732",
+    open: "true",
+    tweoPeople: "₹700",
+    phone: "+91 1234567890",
+    category: "Pizza, Italian Pasta, Fast Food",
+    PriceForOne: "₹300 for one",
+    deliveryTime: "30 min",
+    Gallery: [
+      { imageScr: "Image/la-Phinoz-pizza-image-01.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-02.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-03.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-04.avif" },
+    ],
+
+    mealCategory: [
+      {
+        categoryName: "Pizza",
+        meals: [
+          {
+            mealName: "Half N Half Pizza",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: " Veg Half N Half Pizza Large (33cm)",
+                price: "₹289",
+                dishDesc:
+                  "'Paneer, Onions , Capsicum, Mushrooms, Olives, Baby Corn with Loads of Cheese'",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Nargheruta Pizza (personal Giant Slice (22.5cm)",
+                price: "₹190",
+                dishDesc:
+                  "A Classic cheesy Margarita. Cant go wrong. [fat-14.3 per 100g, protein-12.8 per",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Paneer 65 (personal Giant Slice (22.5cm)",
+                price: "₹264",
+                dishDesc:
+                  "A Combination of Paneer, Onions, Capsicum, Red Paprika, Paneer 65 with Loads of Cheese",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Half N Half Pizza Large (33cm)",
+                price: "₹150",
+                dishDesc: "",
+              },
+            ],
+          },
+          {
+            mealName: "Garlic Bread",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Plain Garlic Bread",
+                price: "₹108",
+                dishDesc:
+                  "|1 Pc || The quintessential garlic bread side for your pizza. [Fat-14.3 per 100g, Protein-12.8 per 100g, carbs-50.0 per 100g]",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Single Veg Pack of 4",
+                price: "₹210",
+                dishDesc:
+                  "Onion Pizza + Tamatoes Pizza + Caspsicum Pizza + Sweet Corn Pizza",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        categoryName: "Dotd",
+        meals: [
+          {
+            mealName: "Pizzza",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Capsicum Pizza Regular",
+                price: "₹150",
+                dishDesc: "Fresh And Cripsy Capsicum to Perfection",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "R2",
+    imageSrc: "Image/Restautant-Spice-villa.avif",
+    offerText: "₹150 OFF",
+    RestaurantsName: "Spice-Villa",
+    rating: "4.3",
+    TotalRating: "1346",
+    Diningrating: "4.9",
+    DeliveryOut: "false",
+    TotalDinigrating: "111",
+    open: "false",
+    tweoPeople: "₹1100",
+    phone: "+91 1234567730",
+    category: "Punhjabi, Indian, North Indian",
+    PriceForOne: "₹200 for one",
+    deliveryTime: "$0 min",
+    Gallery: [
+      { imageScr: "Image/Space-Villa_image01.avif" },
+      { imageScr: "Image/Space-Villa_image02.avif" },
+      { imageScr: "Image/Space-Villa_image03.avif" },
+      { imageScr: "Image/Space-Villa_image04.avif" },
+    ],
+
+    mealCategory: [
+      {
+        categoryName: "Soup",
+        meals: [
+          {
+            mealName: "Soup",
+            disOption: [
+              {
+                dishImage: "Image/Space-Villa_image01.avif",
+                dishName: "Hot & Sour Soup (230ml)",
+                price: "₹300",
+                dishDesc:
+                  "Savory, mildly spicy and tangy & delicious soup with vegetables",
+              },
+              {
+                dishImage: "Image/Space-Villa_image02.avif",
+                dishName: "Smoked Tomato Soup (230ml)",
+                price: "₹390",
+                dishDesc:
+                  "Smoked Tangy fresh tomato puree flavored with basil & parmesan",
+              },
+            ],
+          },
+          {
+            mealName: "Salad & Accompaniments",
+            disOption: [
+              {
+                dishImage: "Image/Space-Villa_Salads01.avif",
+                dishName: "Desi Green Salad (150 gms)",
+                price: "₹190",
+                dishDesc:
+                  "Classic Indian salad with sliced, carrots, cucumber, tomato, onion & lemon dressing",
+              },
+            ],
+          },
+          {
+            mealName: "Salad & Accompaniments",
+            disOption: [
+              {
+                dishImage: "Image/Space-Villa_Salads01.avif",
+                dishName: "Desi Green Salad (150 gms)",
+                price: "₹190",
+                dishDesc:
+                  "Classic Indian salad with sliced, carrots, cucumber, tomato, onion & lemon dressing",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        categoryName: "Dotd",
+        meals: [
+          {
+            mealName: "Pizzza",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Capsicum Pizza Regular",
+                price: "₹150",
+                dishDesc: "Fresh And Cripsy Capsicum to Perfection",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "R2",
+    imageSrc: "Image/Restaurants_Products1.avif",
+    offerText: "₹200 OFF",
+    RestaurantsName: "La Pino's Pizza",
+    rating: "4.1",
+    TotalRating: "1156",
+    Diningrating: "4.3",
+    Deliveryrating: "4.0",
+    DeliveryOut: "true",
+    TotalDinigrating: "732",
+    open: "true",
+    tweoPeople: "₹700",
+    phone: "+91 1234567890",
+    category: "Pizza, Italian Pasta, Fast Food",
+    PriceForOne: "₹300 for one",
+    deliveryTime: "30 min",
+    Gallery: [
+      { imageScr: "Image/la-Phinoz-pizza-image-01.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-02.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-03.avif" },
+      { imageScr: "Image/la-Phinoz-pizza-image-04.avif" },
+    ],
+
+    mealCategory: [
+      {
+        categoryName: "Pizza",
+        meals: [
+          {
+            mealName: "Half N Half Pizza",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: " Veg Half N Half Pizza Large (33cm)",
+                price: "₹289",
+                dishDesc:
+                  "'Paneer, Onions , Capsicum, Mushrooms, Olives, Baby Corn with Loads of Cheese'",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Nargheruta Pizza (personal Giant Slice (22.5cm)",
+                price: "₹190",
+                dishDesc:
+                  "A Classic cheesy Margarita. Cant go wrong. [fat-14.3 per 100g, protein-12.8 per",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Paneer 65 (personal Giant Slice (22.5cm)",
+                price: "₹264",
+                dishDesc:
+                  "A Combination of Paneer, Onions, Capsicum, Red Paprika, Paneer 65 with Loads of Cheese",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Half N Half Pizza Large (33cm)",
+                price: "₹150",
+                dishDesc: "",
+              },
+            ],
+          },
+          {
+            mealName: "Garlic Bread",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Plain Garlic Bread",
+                price: "₹108",
+                dishDesc:
+                  "|1 Pc || The quintessential garlic bread side for your pizza. [Fat-14.3 per 100g, Protein-12.8 per 100g, carbs-50.0 per 100g]",
+              },
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Single Veg Pack of 4",
+                price: "₹210",
+                dishDesc:
+                  "Onion Pizza + Tamatoes Pizza + Caspsicum Pizza + Sweet Corn Pizza",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        categoryName: "Dotd",
+        meals: [
+          {
+            mealName: "Pizzza",
+            disOption: [
+              {
+                dishImage:
+                  "https://b.zmtcdn.com/data/dish_photos/531/70beee3f089b81f5953672eb0370a531.jpg",
+                dishName: "Capsicum Pizza Regular",
+                price: "₹150",
+                dishDesc: "Fresh And Cripsy Capsicum to Perfection",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
